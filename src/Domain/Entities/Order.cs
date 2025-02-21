cc04fd61103b196a1e3138e6484ad9cf5a35af8a
kauatwn <kaualopescontato@gmail.com>
2025-02-21 15:05:05 -0300

    Adiciona classe Order

---
+++ src/Domain/Entities/Order.cs
@@ -0,0 +1,10 @@
+﻿using Domain.Enums;
+
+namespace Domain.Entities;
+
+public class Order(double weight, double distance, ShippingMethod shippingMethod)
+{
+    public double Weight { get; set; } = weight;
+    public double Distance { get; set; } = distance;
+    public ShippingMethod ShippingMethod { get; set; } = shippingMethod;
+}
