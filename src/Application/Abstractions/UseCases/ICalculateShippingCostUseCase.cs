e921be2ea84a0e1851997e0fd8afbd61c21b10ee
kauatwn <kaualopescontato@gmail.com>
2025-02-23 12:51:11 -0300

    Adiciona interface ICalculateShippingCostUseCase

---
+++ src/Application/Abstractions/UseCases/ICalculateShippingCostUseCase.cs
@@ -0,0 +1,9 @@
+﻿using Application.DTOs;
+using Domain.Entities;
+
+namespace Application.Abstractions.UseCases;
+
+public interface ICalculateShippingCostUseCase
+{
+    ShippingCostResponse Execute(Order order);
+}
