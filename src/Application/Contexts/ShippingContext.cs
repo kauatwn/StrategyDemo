afa8bbde412edff05f5f21698d0b215151923a8c
kauatwn <kaualopescontato@gmail.com>
2025-02-23 12:51:52 -0300

    Adiciona classe ShippingContext

---
+++ src/Application/Contexts/ShippingContext.cs
@@ -0,0 +1,23 @@
+﻿using Application.Abstractions.Contexts;
+using Domain.Entities;
+using Domain.Enums;
+using Domain.Interfaces.Factories;
+using Domain.Interfaces.Strategies;
+
+namespace Application.Contexts;
+
+public class ShippingContext(IShippingStrategyFactory factory) : IShippingContext
+{
+    private IShippingStrategyFactory Factory { get; } = factory;
+    public IShippingStrategy Strategy { get; private set; } = null!;
+
+    public void SetStrategy(ShippingMethod method)
+    {
+        Strategy = Factory.Create(method);
+    }
+
+    public double CalculateShippingCost(Order order)
+    {
+        return Strategy.Calculate(order);
+    }
+}
