7a7b2ed86bd3e3f2931e428ff448ed6246bd45de
kauatwn <kaualopescontato@gmail.com>
2025-02-23 12:52:38 -0300

    Adiciona classe CalculateShippingCostUseCase

---
+++ src/Application/UseCases/CalculateShippingCostUseCase.cs
@@ -0,0 +1,21 @@
+﻿using Application.Abstractions.Contexts;
+using Application.Abstractions.UseCases;
+using Application.DTOs;
+using Domain.Entities;
+
+namespace Application.UseCases;
+
+public class CalculateShippingCostUseCase(IShippingContext context) : ICalculateShippingCostUseCase
+{
+    private IShippingContext Context { get; } = context;
+
+    public ShippingCostResponse Execute(Order order)
+    {
+        Context.SetStrategy(order.ShippingMethod);
+
+        double cost = Context.CalculateShippingCost(order);
+        var message = $"The {order.ShippingMethod.ToString().ToLower()} shipping cost is {cost:C}";
+
+        return new ShippingCostResponse(message, cost, order.ShippingMethod.ToString());
+    }
+}
