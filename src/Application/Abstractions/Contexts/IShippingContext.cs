85384278e918847cf5463d59b23da14672003f04
kauatwn <kaualopescontato@gmail.com>
2025-02-23 12:49:17 -0300

    Adiciona interface IShippingContext

---
+++ src/Application/Abstractions/Contexts/IShippingContext.cs
@@ -0,0 +1,10 @@
+﻿using Domain.Entities;
+using Domain.Enums;
+
+namespace Application.Abstractions.Contexts;
+
+public interface IShippingContext
+{
+    void SetStrategy(ShippingMethod method);
+    double CalculateShippingCost(Order order);
+}
