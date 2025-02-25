d2d341cdbfb932a698468b64f332f9ca55cc3b49
kauatwn <kaualopescontato@gmail.com>
2025-02-25 14:38:15 -0300

    Adiciona classe ShippingStrategyFactory

---
+++ src/Infrastructure/Factories/ShippingStrategyFactory.cs
@@ -0,0 +1,16 @@
+﻿using Domain.Enums;
+using Domain.Interfaces.Factories;
+using Domain.Interfaces.Strategies;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Infrastructure.Factories;
+
+public class ShippingStrategyFactory(IServiceProvider serviceProvider) : IShippingStrategyFactory
+{
+    private IServiceProvider ServiceProvider { get; } = serviceProvider;
+
+    public IShippingStrategy? Create(ShippingMethod method)
+    {
+        return ServiceProvider.GetKeyedService<IShippingStrategy>(method);
+    }
+}
