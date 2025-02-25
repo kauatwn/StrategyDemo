a92c80c5183f56615e60078a13237e64eec790a8
kauatwn <kaualopescontato@gmail.com>
2025-02-25 14:38:56 -0300

    Adiciona classe ExpressShippingStrategy

---
+++ src/Infrastructure/Strategies/Shipping/ExpressShippingStrategy.cs
@@ -0,0 +1,15 @@
+﻿using Domain.Entities;
+using Domain.Interfaces.Strategies;
+
+namespace Infrastructure.Strategies.Shipping;
+
+public class ExpressShippingStrategy : IShippingStrategy
+{
+    private const double CostPerKg = 2.0;
+    private const double CostPerKm = 1.0;
+
+    public double Calculate(Order order)
+    {
+        return order.Weight * CostPerKg + order.Distance * CostPerKm;
+    }
+}
