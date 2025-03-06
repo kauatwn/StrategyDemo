50ad164be7b375810125e0a0de19d7390b9c27bd
kauatwn <kaualopescontato@gmail.com>
2025-03-06 15:09:40 -0300

    Adiciona classe OrdersController

---
+++ src/API/Controllers/OrdersController.cs
@@ -0,0 +1,20 @@
+﻿using Application.Abstractions.UseCases;
+using Application.DTOs;
+using Domain.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class OrdersController : ControllerBase
+{
+    [HttpPost("CalculateShippingCost")]
+    [ProducesResponseType<ShippingCostResponse>(StatusCodes.Status200OK)]
+    public IActionResult CalculateShippingCost(ICalculateShippingCostUseCase useCase, Order order)
+    {
+        ShippingCostResponse result = useCase.Execute(order);
+
+        return Ok(result);
+    }
+}
