b424e5f150aad538a33625eecc4ef8db39244027
kauatwn <kaualopescontato@gmail.com>
2025-03-25 09:29:53 -0300

    Renomeia parâmetro do construtor Order para evitar redundância em argumentos nomeados dos testes

--- src/Domain/Entities/Order.cs
+++ src/Domain/Entities/Order.cs
@@ -2,9 +2,9 @@
 
 namespace Domain.Entities;
 
-public class Order(double weight, double distance, ShippingMethod shippingMethod)
+public class Order(double weight, double distance, ShippingMethod method)
 {
     public double Weight { get; set; } = weight;
     public double Distance { get; set; } = distance;
-    public ShippingMethod ShippingMethod { get; set; } = shippingMethod;
+    public ShippingMethod ShippingMethod { get; set; } = method;
 }
