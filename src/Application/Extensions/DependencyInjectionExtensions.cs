9e699f18851ba3e1350c5a6e1ffcfddf7484f6e7
kauatwn <kaualopescontato@gmail.com>
2025-04-17 20:10:27 -0300

    Adiciona registro de dependências

---
+++ src/Application/Extensions/DependencyInjectionExtensions.cs
@@ -0,0 +1,28 @@
+﻿using Application.Abstractions.Contexts;
+using Application.Abstractions.UseCases;
+using Application.Contexts;
+using Application.UseCases;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Application.Extensions;
+
+public static class DependencyInjectionExtensions
+{
+    public static IServiceCollection AddApplication(this IServiceCollection services)
+    {
+        AddContexts(services);
+        AddUseCases(services);
+
+        return services;
+    }
+
+    private static void AddContexts(IServiceCollection services)
+    {
+        services.AddScoped<IShippingContext, ShippingContext>();
+    }
+
+    private static void AddUseCases(IServiceCollection services)
+    {
+        services.AddScoped<ICalculateShippingCostUseCase, CalculateShippingCostUseCase>();
+    }
+}
---
+++ src/Infrastructure/Extensions/DependencyInjectionExtensions.cs
@@ -0,0 +1,30 @@
+﻿using Domain.Enums;
+using Domain.Interfaces.Factories;
+using Domain.Interfaces.Strategies;
+using Infrastructure.Factories;
+using Infrastructure.Strategies.Shipping;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Infrastructure.Extensions;
+
+public static class DependencyInjectionExtensions
+{
+    public static IServiceCollection AddInfrastructure(this IServiceCollection services)
+    {
+        AddStrategies(services);
+        AddFactories(services);
+
+        return services;
+    }
+
+    private static void AddStrategies(IServiceCollection services)
+    {
+        services.AddKeyedTransient<IShippingStrategy, StandardShippingStrategy>(ShippingMethod.Standard);
+        services.AddKeyedTransient<IShippingStrategy, ExpressShippingStrategy>(ShippingMethod.Express);
+    }
+
+    private static void AddFactories(IServiceCollection services)
+    {
+        services.AddSingleton<IShippingStrategyFactory, ShippingStrategyFactory>();
+    }
+}
