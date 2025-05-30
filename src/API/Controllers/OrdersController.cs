62a6ad88ddf3873bdac3119a8261f7cecdb3a0ad
kauatwn <kaualopescontato@gmail.com>
2025-05-30 11:20:49 -0300

    Refatora OrdersController para retornar ShippingCostResponse com base em ShippingCostResult

--- src/API/Controllers/OrdersController.cs
+++ src/API/Controllers/OrdersController.cs
@@ -1,4 +1,5 @@
-﻿using Application.Abstractions.UseCases;
+﻿using API.DTOs.Responses;
+using Application.Abstractions.UseCases;
 using Application.DTOs;
 using Domain.Entities;
 using Microsoft.AspNetCore.Mvc;
@@ -11,10 +12,12 @@ public class OrdersController : ControllerBase
 {
     [HttpPost("CalculateShippingCost")]
     [ProducesResponseType<ShippingCostResponse>(StatusCodes.Status200OK)]
-    public IActionResult CalculateShippingCost(ICalculateShippingCostUseCase useCase, Order order)
+    public ActionResult<ShippingCostResponse> CalculateShippingCost(ICalculateShippingCostUseCase useCase, Order order)
     {
-        ShippingCostResponse result = useCase.Execute(order);
+        ShippingCostResult result = useCase.Execute(order);
+        ShippingCostResponse response = new($"The {result.ShippingMethod.ToLower()} shipping cost is {result.Cost:C}",
+            result.Cost, result.ShippingMethod);
 
-        return Ok(result);
+        return Ok(response);
     }
 }
